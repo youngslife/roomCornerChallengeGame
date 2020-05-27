ec71f1c05ce63ee658b2a19721072c1e0af9ccbf
김은영 <eyeykim12@gmail.com>
2020-05-27 13:37:42 +0900

    S02P31A304-54 # Squat모델 Count+1 공격 연결

--- front/front-vue/src/components/predictions/SquatPrediction.vue
+++ front/front-vue/src/components/predictions/SquatPrediction.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     {{ bestPrediction.className }}: {{ bestPrediction.probability.toFixed(2) }}
-    <div id="test">{{ test }}</div>
+    <div id="test">{{ test() }}</div>
   </div>
 </template>
 
@@ -29,6 +29,7 @@ export default {
         time: 0,
       },
       befAction: "",
+      count: 0,
     };
   },
   computed: {
@@ -42,34 +43,26 @@ export default {
         { probability: 0 }
       );
     },
-    test() {
-    //   const test2 =
-    //     this.bestPrediction.probability > 0.9
-    //       ? this.bestPrediction.className === "Class 2"
-    //         ? "jump"
-    //         : this.bestPrediction.className === "Class 3"
-    //         ? "walk"
-    //         : "stand"
-    //       : "None";
-    //   return test2;
-        if(this.bestPrediction.className == "stand" || this.bestPrediction.className == "step2"){
-            if(this.bestPrediction.probability == 1.00){
-                return "jump"
-            }
-        } else if (this.bestPrediction.className == "sit"){
-            return "walk"
-        }
-        return "stand"
-    },
+    // test() {
+    //     if(this.bestPrediction.className == "stand" || this.bestPrediction.className == "step2"){
+    //         if(this.bestPrediction.probability == 1.00){
+    //             return "jump"
+    //         }
+    //     } else if (this.bestPrediction.className == "sit"){
+    //         return "walk"
+    //     }
+    //     return "stand"
+    // },
+
   },
   mounted() {
     this.action(0);
   },
   methods: {
     action(time) {
-      if (this.startTime.type !== this.test) {
+      if (this.startTime.type !== this.test()) {
         this.startTime.time = Date.now();
-        this.startTime.type = this.test;
+        this.startTime.type = this.test();
       }
       if (Date.now() - this.startTime.time >= 1500) {
         this.$emit("child", this.startTime.type);
@@ -77,6 +70,27 @@ export default {
       setTimeout(() => {
         this.action(time + 100);
       }, 100);
+    },
+  test() {
+        if(this.bestPrediction.className == "stand" || this.bestPrediction.className == "step2"){
+            if(this.bestPrediction.probability > 0.97){
+              if(this.status == "squat"){
+                this.count++;
+                console.log(this.count);
+              }
+              this.status = "stand"
+              // console.log(this.status)
+                return this.count
+            }
+        }else if (this.bestPrediction.className == "sit"){
+            if(this.bestPrediction.probability > 0.97){
+              if(this.status == "stand"){
+                this.status = "squat";
+                
+              }
+            }
+        }
+        return this.count
     },
   },
 };
